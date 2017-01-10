995b84f7e3209dd382fdb2ad8960d0e9062b6679
Michael Khoroshun <michael.khoroschun@gmail.com>
2017-01-10 17:10:54 +0200

    Add third task
    * Create task_3.js
    * Update README.md

---
+++ task_3.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const A = [1, -2, -3, 4, -5, 6, 7, -8, 9, -10, 11, -12, 13, 14, -15];
+
+console.log( calcSum(A) );
+
+function calcSum(A) {
+  var sum = 0;
+
+  for (var i = 0; i < A.length; i+=2)
+  {
+    if (A[i] > 0) {
+      sum += A[i];
+    }
+  }
+  
+  return sum;
+}
